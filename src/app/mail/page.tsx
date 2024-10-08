9f5a6dbfeab2affeef999e953185e273de63142f
Jai-17 <madhukarjai9@gmail.com>
2024-10-09 03:16:10 +0530

    mail dashboard skeleton added

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Mail from "./mail";
 
 const MailDashboard = () => {
   return (
     <div>
-      Mail Dashboard
+      <Mail
+        defaultLayout={[20, 32, 48]}
+        navCollapsedSize={4}
+        defaultCollapsed={false}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default MailDashboard;
